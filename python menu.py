495d58fecac206788571be9092b350f2e8e3dda0
laurabsouza2002-star <laurabsouza2002@gmail.com>
2026-06-07 15:01:53 -0300

    adicionando o arquivo do menu

---
+++ python menu.py
@@ -0,0 +1,25 @@
+#MENU DE INICIALIZAÇÃO: 
+import time
+def linhas():
+    print("=" *50)
+
+linhas()
+print('🧡 GUARDIÕES DA INFÂNCIA - MAIO LARANJA 🧡'.center(50))
+linhas()
+
+nome = input("Digite seu nome: ")
+linhas()
+print(f"Seja bem-vindo(a), {nome}! Sua missão é ajudar na conscientização!" .upper())
+time.sleep (2.0)
+
+linhas()
+print('CENTRAL DOS GUARDIÕES:'.center(50))
+linhas()
+
+print("1 - Orientações e prevenção")
+print("2 - Canal de denúncia")
+print("3 - Mini-game")
+print("4 - Informações sobre a campanha")
+print("5 - Encerrar missão")
+
+opcao = int(input("\nEscolha uma opção: "))
